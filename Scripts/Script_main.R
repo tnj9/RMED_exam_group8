0e0f313822816c1a86dc185748dbe4566f82569a
Kathrine Brun <bruk@ihelse.net>
2024-09-09 14:36:37 +0200

    Exploring data

--- Scripts/Script_main.R
+++ Scripts/Script_main.R
@@ -7,3 +7,27 @@
 #               
 # Project: RMED_exam_group8
 #-------------------------------------------###
+
+#load libraries
+library(tidyverse)
+library(readr)
+library(here)
+library(skimr)
+
+read_tsv("exam_dataset.txt")
+#Got ERROR
+
+read_delim(here("DATA","exam_dataset.txt"))
+#Chose delim as the others in the group. Downloaded library "here"and revised the code - worked! 
+
+data<-read_delim(here("DATA","exam_dataset.txt"))
+data
+
+#exploring data
+head(data)
+tail(data)
+summary(data)
+glimpse(data)
+skimr::skim(data)
+
+
