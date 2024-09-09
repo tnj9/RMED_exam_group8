c1768f2b39ca230aec1bea1b1c05b8ba943649eb
tnj <tje029@uib.no>
2024-09-09 13:50:25 +0200

    Added description (authors, date, etc)

---
+++ Scripts/Script_main.R
@@ -0,0 +1,9 @@
+#----Main Script-------------------------####
+# Date: 09.09.24
+# Author: Eirik Røys,Tuva N Jensen, Kathrine Brun
+# Filename: Script_main.R    
+# Description:  
+#               
+#               
+# Project: RMED_exam_group8
+#-------------------------------------------###
