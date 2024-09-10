472170e34ebd7c3ca128541bfa0097a9a8d23ea3
eirroy <eirik.asen.roys@helse-bergen.no>
2024-09-10 17:58:17 +0200

    Commit before pull

--- Scripts/Script_main.R
+++ Scripts/Script_main.R
@@ -42,7 +42,6 @@ data <- data %>%
 
 # View the updated data
 colnames(data)
-<<<<<<< HEAD
 
 # Combine year and month into a single column as a date format.
 data$year_month <- dmy(paste("01", data$month, data$year, sep = "-"))
@@ -50,6 +49,5 @@ data$year_month <- dmy(paste("01", data$month, data$year, sep = "-"))
 # Remove year and month columns
 data$year <- NULL
 data$month <- NULL
-=======
 >>>>>>> 019ec0b24d14a94a648a1885274f234176b7dca3
 
