1e69d3d14b8c9fba81ff44e3212c95d3cdb201e3
eirroy <eirik.asen.roys@helse-bergen.no>
2024-09-11 14:47:41 +0200

    Added libraries

--- Scripts/Script_analyze.R
+++ Scripts/Script_analyze.R
@@ -4,4 +4,8 @@
 #   K Does the preoperative pain change with age of the patients?
 #   T Do BMI and age have a linear relationship?
 
+#Load libraries
+library(here)
+library(tidyverse)
+
 data <- read.csv2(here("DATA", "clean_data.csv"))
