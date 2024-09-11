625cb0f6054bc658e83864b45c775dc93b4d39ff
eirroy <eirik.asen.roys@helse-bergen.no>
2024-09-11 21:11:31 +0200

    Flipped plots

--- Scripts/Script_analyze.R
+++ Scripts/Script_analyze.R
@@ -16,24 +16,31 @@ data <- read.csv2(here("DATA", "clean_data.csv"))
 # Set 'treat' as factor
 data$treat <- factor(data$treat)
 
-# Boxplot Age: vs. Treat
+# Boxplot Age vs. Treat (flipped)
 boxplot_treat <- ggplot(data, aes(x = treat, y = age, fill = treat)) +
   geom_boxplot(show.legend = FALSE) +
   scale_fill_manual(values = c("0" = "#FBADA7", "1" = "#64B8B8")) +
   scale_y_continuous(breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90), limits = c(10, 90)) +
   labs(title = "Box-plot") +
-  theme_classic() 
+  theme_classic() +
+  coord_flip()  
 
-# Histogram: Age vs. Treat
+# Histogram Age vs. Treat (flipped)
 histogram_treat <- ggplot(data, aes(y = age, fill = treat)) +
   geom_histogram(binwidth = 10, position = "dodge", color = "black") +
   scale_fill_manual(values = c("0" = "#FBADA7", "1" = "#64B8B8")) +
   scale_y_continuous(breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90), limits = c(10, 90)) +
   labs(title = "Histogram") +
-  theme_classic() 
+  theme_classic() +
+  coord_flip()  
 
 # Combine plots 
-Age_vs_treat_plot <- (boxplot_treat + histogram_treat) + plot_layout(guides = 'collect')
+Age_vs_treat_plot <- (boxplot_treat / histogram_treat) +
+  plot_layout(guides = 'collect') + 
+  plot_annotation(
+    title = "Age vs. Treatment",
+    theme = theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+  )
 Age_vs_treat_plot
 
 #----- Does the age distribution of the patients depend on their sex (`gender`)?--------
@@ -44,7 +51,8 @@ boxplot_gender <- ggplot(data, aes(x = gender, y = age, fill = gender)) +
   scale_fill_manual(values = c("Female" = "#FBADA7", "Male" = "#64B8B8")) +
   scale_y_continuous(breaks = c(10, 20, 30, 40, 50, 60, 70, 80, 90), limits = c(10, 90)) +
   labs(title = "Box-plot") +
-  theme_classic() 
+  theme_classic() +
+  coord_flip() 
 
 # Histogram: Age vs. Gender
 histogram_gender <- ggplot(data, aes(y = age, fill = gender)) +
@@ -52,8 +60,17 @@ histogram_gender <- ggplot(data, aes(y = age, fill = gender)) +
   scale_fill_manual(values = c("Female" = "#FBADA7", "Male" = "#64B8B8")) +
   scale_y_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90), limits = c(10, 90)) +
   labs(title = "Histogram") +
-  theme_classic() 
+  theme_classic() +
+  coord_flip() 
 
 # Combine plots 
-Age_vs_gender_plot <- (boxplot_gender + histogram_gender) + plot_layout(guides = 'collect')
+Age_vs_gender_plot <- (boxplot_gender / histogram_gender) + 
+  plot_layout(guides = 'collect') + 
+  plot_annotation(
+    title = "Age vs. Gender",
+    theme = theme(plot.title = element_text(hjust = 0.5, face = "bold"))
+  )
+
+# Display the plot
 Age_vs_gender_plot
+
